c9553dd0688400e012caebc67d1e1c2b24b7fcc2
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-17 19:38:45 +0300

    test the updated SDK with both Write and Read operations

--- TestMauiRfidControllers/Hardware/VF747pController.cs
+++ TestMauiRfidControllers/Hardware/VF747pController.cs
@@ -9,14 +9,12 @@ public class VupVF747pController : RfidController
     private const int MAX_POWER = 27;
     private NetVupReader reader;
     private readonly string ipAddress;
-	private readonly ILogger logger;
 
     protected override string Device => "VF747p";
 
-    public VupVF747pController(string ipAddress, ILogger logger, TimeSpan? throttle = null) : base(throttle)
+    public VupVF747pController(string ipAddress, TimeSpan? throttle = null) : base(throttle)
     {
         this.ipAddress = ipAddress;
-		this.logger = logger;
 		reader = new NetVupReader(ipAddress, 1969, transport_protocol.tcp);
     }
 
@@ -130,7 +128,7 @@ public class VupVF747pController : RfidController
                 this.Reconnect();
                 reconnectTimer.Stop();
                 sb.AppendLine($"Reconnected after '{reconnectTimer.ElapsedMilliseconds}'");
-                this.logger.Log("VF747p-disconnected", sb.ToString());
+                //this.logger.Log("VF747p-disconnected", sb.ToString());
             }
 
             if (tagsBytes.Success)
---
+++ TestMauiRfidControllers/Reader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using Test.Entities;
+using Test.Hardware;
+
+namespace Test_RfidControllers;
+
+public class Reader
+{
+    private VupVF747pController VF747PController;
+    private ConcurrentQueue<string> _readData = new ConcurrentQueue<string>();
+    public Reader()
+    {
+        VF747PController = new VupVF747pController("192.168.68.128", TimeSpan.FromMilliseconds(10));
+    }
+
+    public async Task StartReading(bool readFlag=true)
+    {
+        if (readFlag)
+        {
+            var data = await Task.Run(VF747PController.StartReading);
+            foreach (var item in data)
+            {
+                _readData.Enqueue(item);
+            }
+        }
+        else
+        {
+            VF747PController.StopReading();
+        }
+
+    }
+
+    public IEnumerable<string> ProcessTags()
+    {
+        foreach (var tagData in _readData)
+        {
+            var number = int.Parse(tagData.Substring(0, 3));
+            //process here?
+            yield return $"Tag Number: {number} detected!";
+        }
+    }
+
+    //public void ProcessTags()
+    //{
+    //    foreach (var tagData in _readData)
+    //    {
+    //        var number = int.Parse(tagData.Substring(0, 3));
+    //        var timestamp = new Timestamp(DateTime.Now);
+    //        var snapshot = new Snapshot(number, StaticOptions.GetRfidSnapshotType(), SnapshotMethod.RFID, timestamp);
+    //        //process here?
+    //    }
+    //}
+}
